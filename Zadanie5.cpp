b0c313e52f3dcd9e5b9406ac51fd63bd38680d6b
frai32 <frai32@mail.ru>
2020-06-28 22:15:43 +1000

    5Zadanie
    Добавил 5 задание

--- Zadanie5.cpp
+++ Zadanie5.cpp
@@ -8,7 +8,7 @@ int main()
 	int x[3];
 	int y[3];
 
-	double dotNumber;
+	
 
 	std::cout << "Введите координаты: "<< std::endl;
 
@@ -21,27 +21,18 @@ int main()
 		std::cin >> y[i];
 	}
 
-	std::cout << "введите колличество точек х: ";
-	std::cin >> dotNumber;
 	
-	if (dotNumber >= 1)
+	
+	for (float t = 0; t <= 1; t += 0.05)
 	{
-		double dt = 1 / dotNumber;
-		double t = -dt;
-
-		for (int i = 0; i <= dotNumber; i++)
-		{
-			t += dt;
-
-			double xb = (1 - t) * (1 - t) * x[0] + 2 * (1 - t) * t * x[1] + t * t * x[2];
-			double yb = (1 - t) * (1 - t) * y[0] + 2 * (1 - t) * t * y[1] + t * t * y[2];
-			std::cout << xb << "  " << yb << std::endl;
-		}
+		double oneMinusT = (1 - t);
+		double xb = oneMinusT * oneMinusT * x[0] + 2 * oneMinusT * t * x[1] + t * t * x[2];
+		double yb = oneMinusT * oneMinusT * y[0] + 2 * oneMinusT * t * y[1] + t * t * y[2];
+		std::cout << xb << "  " << yb << std::endl;
 	}
-	else
-	{
-		std::cout << "колличество точек меньше либо равно 0";
-	}
+
+	
+	
 
 
 	return 0;
